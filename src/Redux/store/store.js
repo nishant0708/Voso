519aa1435f7c9a955b1c0b0c47ca7714aee3a0a5
Nishant Kaushal <nishantkaushal0708@gmail.com>
2024-04-19 10:44:32 +0530

    merged complete

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,24 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import topUsersSlice from '../slicer/slicer';
-<<<<<<< HEAD
 import month from "../slicer/Monthlyslicer"
 import authSlicer from '../slicer/authSlicer';
 import login_mobileReducer from '../slicer/login_mobileSlicer';
-=======
-import month from "../slicer/Monthlyslicer";
+
 import usersSlice from "../slicer/userList";
->>>>>>> origin/shivanshu
 
 const store = configureStore({
   reducer: {
     topUsers:topUsersSlice,
     MonthCount:month,
-<<<<<<< HEAD
     auth:authSlicer,
-    login_mobile: login_mobileReducer
-=======
+    login_mobile: login_mobileReducer,
     usersList: usersSlice,
->>>>>>> origin/shivanshu
   },
   // Other store configurations can go here, such as middleware setup
 });

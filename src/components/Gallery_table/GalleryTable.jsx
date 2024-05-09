fbe13a55d718b5c3dab6b8b19a44c181cd4fbb5d
nishant0708 <nishantkaushal0708@gmail.com>
2024-05-09 16:37:17 +0530

    skeleton

--- src/components/Gallery_table/GalleryTable.jsx
+++ src/components/Gallery_table/GalleryTable.jsx
@@ -24,7 +24,7 @@ const GalleryTable = () => {
     dispatch(fetchgallery({ userId }));
   }, [dispatch, userId]);
 
-  if ( status === 'loading') {
+  if (status === 'loading') {
     return <GalleryviewSkeleton />;
   }
 
--- src/components/Skeletons/DisplayTable.jsx
+++ src/components/Skeletons/DisplayTable.jsx
@@ -22,16 +22,8 @@ const DisplayTable = () => {
         {Array.from({ length: 5 }).map((_, index) => (
           <div className="flex items-center mt-4">
             <div className="flex items-center justify-center mt-4 ">
-              <svg
-                class=" ml-2 w-12 h-12 text-[#CBCBD2] dark:text-gray-700 me-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-              </svg>
-              <div className="w-[190px] bg-[#CBCBD2] dark:bg-[#374151] rounded-lg shadow-md p-4 "></div>
+              <div className="ml-4 w-12 h-12 bg-[#CBCBD2] dark:bg-[#374151] rounded-full shadow-md "></div>
+              <div className="ml-3 w-[190px] bg-[#CBCBD2] dark:bg-[#374151] rounded-lg shadow-md p-4 "></div>
               <div className="ml-2 w-[180px] bg-[#CBCBD2] dark:bg-[#374151] rounded-lg shadow-md p-4 "></div>
               <div className="ml-2.5 w-[130px] bg-[#CBCBD2] dark:bg-[#374151] rounded-lg shadow-md p-4 "></div>
               <div className="ml-2 w-[135px] bg-[#CBCBD2] dark:bg-[#374151] rounded-lg shadow-md p-4 "></div>

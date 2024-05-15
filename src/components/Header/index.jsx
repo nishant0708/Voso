7efe3d9e40e626a0e03f95273ed6e26af6975865
RajeshQuintus <rajesh.quintus@gmail.com>
2024-05-15 15:54:31 +0530

    added search function for mobile view

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -19,7 +19,7 @@ const Header = (props) => {
   const dispatch = useDispatch();
   const location = useLocation();
   const [searchQuery, setSearchQuery] = useState('');
-
+  const [isMobileSearchOpen, setIsMobileSearchOpen] = useState(false);
   // Memoize the debounced function using useCallback
   const debouncedDispatch = useCallback(
     debounce((query) => {
@@ -27,7 +27,18 @@ const Header = (props) => {
     }, 300), // 300 ms delay
     [dispatch], // Dependencies: update if dispatch changes
   );
-
+  // Memoize the debounced function using useCallback
+  const debouncedFetchUser = useCallback(
+    debounce(() => {
+      dispatch(fetchUsers({ page: 1, limit: 20 }));
+    }, 500), // 300 ms delay
+    [dispatch], // Dependencies: update if dispatch changes
+  );
+  useEffect(() => {
+    if (searchQuery === '') {
+      debouncedFetchUser();
+    }
+  }, [searchQuery, debouncedFetchUser]);
   useEffect(() => {
     if (searchQuery) debouncedDispatch(searchQuery); // Call the debounced function when searchQuery changes
   }, [searchQuery, debouncedDispatch]);
@@ -35,9 +46,10 @@ const Header = (props) => {
   const handleChange = (e) => {
     const query = e.target.value;
     setSearchQuery(query);
-    if (!query) {
-      dispatch(fetchUsers({ page: 1, limit: 20 })); // Fetch users immediately when search query is empty
-    }
+  };
+  const handleCloseMobileSearch = () => {
+    setIsMobileSearchOpen(false);
+    setSearchQuery(''); // Clear search query when closing mobile search
   };
   // Define the paths to check against
   const checkPaths = ['/users', '/blogs', '/products'];
@@ -91,47 +103,74 @@ const Header = (props) => {
           {/* <!-- Hamburger Toggle BTN --> */}
         </div>
         {isMatchingPath ? (
-          <div className="hidden sm:block">
-            <form method="POST">
-              <div className="relative">
-                <button className="absolute left-0 top-1/2 -translate-y-1/2">
-                  <svg
-                    className="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M9.16666 3.33332C5.945 3.33332 3.33332 5.945 3.33332 9.16666C3.33332 12.3883 5.945 15 9.16666 15C12.3883 15 15 12.3883 15 9.16666C15 5.945 12.3883 3.33332 9.16666 3.33332ZM1.66666 9.16666C1.66666 5.02452 5.02452 1.66666 9.16666 1.66666C13.3088 1.66666 16.6667 5.02452 16.6667 9.16666C16.6667 13.3088 13.3088 16.6667 9.16666 16.6667C5.02452 16.6667 1.66666 13.3088 1.66666 9.16666Z"
-                      fill=""
-                    />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M13.2857 13.2857C13.6112 12.9603 14.1388 12.9603 14.4642 13.2857L18.0892 16.9107C18.4147 17.2362 18.4147 17.7638 18.0892 18.0892C17.7638 18.4147 17.2362 18.4147 16.9107 18.0892L13.2857 14.4642C12.9603 14.1388 12.9603 13.6112 13.2857 13.2857Z"
-                      fill=""
-                    />
-                  </svg>
-                </button>
+          <div className=" ">
+            <div className="relative border rounded-sm py-2">
+              <button
+                className="absolute left-1 top-1/2 -translate-y-1/2"
+                onClick={() => setIsMobileSearchOpen(!isMobileSearchOpen)}
+              >
+                <svg
+                  className="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M9.16666 3.33332C5.945 3.33332 3.33332 5.945 3.33332 9.16666C3.33332 12.3883 5.945 15 9.16666 15C12.3883 15 15 12.3883 15 9.16666C15 5.945 12.3883 3.33332 9.16666 3.33332ZM1.66666 9.16666C1.66666 5.02452 5.02452 1.66666 9.16666 1.66666C13.3088 1.66666 16.6667 5.02452 16.6667 9.16666C16.6667 13.3088 13.3088 16.6667 9.16666 16.6667C5.02452 16.6667 1.66666 13.3088 1.66666 9.16666Z"
+                    fill=""
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M13.2857 13.2857C13.6112 12.9603 14.1388 12.9603 14.4642 13.2857L18.0892 16.9107C18.4147 17.2362 18.4147 17.7638 18.0892 18.0892C17.7638 18.4147 17.2362 18.4147 16.9107 18.0892L13.2857 14.4642C12.9603 14.1388 12.9603 13.6112 13.2857 13.2857Z"
+                    fill=""
+                  />
+                </svg>
+              </button>
 
-                <input
-                  type="text"
-                  placeholder="Type to search..."
-                  value={searchQuery}
-                  onChange={handleChange}
-                  className="w-full bg-transparent pl-9 pr-4 text-black focus:outline-none dark:text-white xl:w-125"
-                />
-              </div>
-            </form>
+              <input
+                type="text"
+                placeholder="Type to search..."
+                value={searchQuery}
+                onChange={handleChange}
+                className=" hidden sm:block w-full bg-transparent pl-9 pr-4 text-black focus:outline-none dark:text-white xl:w-125"
+              />
+            </div>
           </div>
         ) : (
           <div></div>
         )}
-
+        {window.innerWidth < 500 && isMobileSearchOpen && (
+          <div className="absolute top-full left-0 w-full bg-white shadow-md z-10 py-2 px-4">
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-lg font-semibold">Search</h2>
+              <button onClick={handleCloseMobileSearch} className="w-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  class="text-black dark:text-white"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 5.293a1 1 0 011.414 0L10 8.586l3.293-3.293a1 1 0 111.414 1.414L11.414 10l3.293 3.293a1 1 0 01-1.414 1.414L10 11.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586 10 5.293 6.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+            <input
+              type="text"
+              placeholder="Type to search..."
+              value={searchQuery}
+              onChange={handleChange}
+              className="w-full bg-transparent pl-9 pr-4 text-black focus:outline-none dark:text-white xl:w-125"
+            />
+          </div>
+        )}
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             <DarkModeSwitcher />
